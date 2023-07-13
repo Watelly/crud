473f23d6acacd8f195fb2860aad3f6b9176a7882
Watelly <watelly@hotmail.com>
2023-07-12 21:30:50 -0300

    precoVendamod

--- database/migrations/2023_07_12_232207_create_produtos_table.php
+++ database/migrations/2023_07_12_232207_create_produtos_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('descricao');
             $table->integer('qtd');
             $table->float('precoUnitario');
-            $table->float('precoVenda');
+            $table->float('precoVendaa');
         });
     }
 
