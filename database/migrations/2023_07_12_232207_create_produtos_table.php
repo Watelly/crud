5cbe98ba95eb647a8b81a718bf9c5636990c5195
Watelly <watelly@hotmail.com>
2023-07-27 19:18:36 -0300

    comit

--- database/migrations/2023_07_12_232207_create_produtos_table.php
+++ database/migrations/2023_07_12_232207_create_produtos_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('descricao');
             $table->integer('qtd');
             $table->float('precoUnitario');
-            $table->float('precoVendaa');
+            $table->float('precoVenda');
             $table->timestamps();
         });
     }
